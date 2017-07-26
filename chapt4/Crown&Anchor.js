cc7f0a97b1e1b1c0841428ce68539ba27e0a5ab0
isaccchoi <isaccchoi@naver.com>
2017-07-27 01:01:59 +0900

    chap4

---
+++ chapt4/Crown&Anchor.js
@@ -0,0 +1,16 @@
+// rule
+// start with 50p
+// game is still going, before get the 100p or lose all p
+// game rule is simple, put p -> roll dice -> if correct picture, get p
+// if correct picture, player gets as much as betting p
+
+
+let funds = 50; // start condition
+
+while(funds > 1 && funds < 100) {
+    // bet p
+
+    // roll dice
+
+    // if correct, get p
+}
