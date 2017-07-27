7f0384978c3c70573f3a5da19bb740b8b00d16ee
isaccchoi <isaccchoi@naver.com>
2017-07-27 20:42:26 +0900

    ch4 block statement

---
+++ chapt4/block_statement.js
@@ -0,0 +1,24 @@
+{   // start block statement
+    console.log("statement 1");
+    console.log("statement 2");
+}   // end block statement
+
+console.log("statement 3");
+
+
+let funds = 50; // start condition
+
+while (funds > 1 && funds < 100) {
+    funds = funds + 2; // make 2 step forward
+    funds = funds - 1; // make a step backward
+}
+// most of control statement have block statement,
+// but It's not a necessary condition
+
+// example
+let funds = 50;
+
+while (funds > 1 && funds < 100)
+    funds = funds + 2;
+
+    
